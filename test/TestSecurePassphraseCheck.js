d2a743d2342ca10af706c27a4999523f4549c390
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-10 01:33:31 +0800

    simple test check done

--- test/TestSecurePassphraseCheck.js
+++ test/TestSecurePassphraseCheck.js
@@ -1,15 +1,32 @@
 var SecurePassphraseCheck = artifacts.require("./SecurePassphraseCheck.sol");
+const BN = require('bn.js');
+
 
 contract('SecurePassphraseCheck', function(accounts) {
+  var testAccount = accounts[0];
+  var answer = 'mynameistata';
+  var answerPrivateKey = web3.utils.keccak256(answer);
+  var answerAddress = web3.eth.accounts.privateKeyToAccount(answerPrivateKey).address;
+  var a = new BN(testAccount.substr(2), 16);
+  var { signature } = web3.eth.accounts.sign(a.toBuffer(), answerPrivateKey);
+
+  console.log("Address: " + testAccount);
+  console.log('Answer: ' + answer);
+  console.log("Answer Private Key: " + answerPrivateKey);
+  console.log("Answer Address: " + answerAddress);
+  console.log("Signature: " + signature);
+
   it("should add question correctly", async function() {
     let instance = await SecurePassphraseCheck.deployed();
-    await instance.addQuestion('0xcCde9d432eF8379c7946D85117B4C6Ef7C2dF565', accounts[0], {from: accounts[0]})
-    let answerAddress = await instance.answerAddress.call();
-    assert.strictEqual(answerAddress, '0xcCde9d432eF8379c7946D85117B4C6Ef7C2dF565');
+    await instance.addQuestion(answerAddress, accounts[0], {from: testAccount})
+    let res = await instance.answerAddress.call();
+    assert.strictEqual(res, answerAddress);
   });
-  it("should check answer correct", async function() {
+  it("should check passphrase correctly", async function() {
     let instance = await SecurePassphraseCheck.deployed();
-    let check = await instance.checkAnswer.call('0x5dedb72af469c493eeb274d19c0ff9a4d9ffc26e783276d5b6d297badebec83a70d920d26bb3702d8c2d9e74dbc1a36e65ed234831b0c29cec7f4f480eb224d71c', {from: accounts[0]})
-    assert.strictEqual(check, true);
+    let check1 = await instance.checkAnswer.call(signature, {from: testAccount})
+    assert.strictEqual(check1, true);
+    let check2 = await instance.checkAnswer.call('0x6dedb72af469c493eeb274d19c0ff9a4d9ffc26e783276d5b6d297badebec83a70d920d26bb3702d8c2d9e74dbc1a36e65ed234831b0c29cec7f4f480eb224d71c', {from: testAccount})
+    assert.strictEqual(check2, false);
   });
 });
