e0eb811b24cb53e8344858a6995eecffa599b3f5
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-09 23:25:58 +0800

    add openzeppelin-solidity

--- truffle-config.js
+++ truffle-config.js
@@ -12,10 +12,13 @@
  *   },
  */
 
-networks: {
-  development: {
-    host: '127.0.0.1',
-    port: 7545,
-    network_id: '*' // Match any network id
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*", // Match any network id
+      gas: 4600000
+    }
   }
-}
+};
