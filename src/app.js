455e350154fc85371b0c101200c4f4a7f66c3baa
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-12 01:54:03 +0800

    added web3 but has version bug

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Web3 from 'web3';
+import config from './config.js';
+import { abi as scpContractABI } from '../build/contracts/SecurePassphraseCheck.json';
+
 class App extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { liked: false };
+      this.state = { loading: true };
+      this.web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+      this.web3.eth.getAccounts().then((accounts) => {
+        this.setState({
+          loading: !(accounts.length > 0 )
+        });
+      });
+      this.setupContracts();
+    }
+
+    setupContracts() {
+      this.scpContracct = new web3.eth.Contract(scpContractABI, config.spcContractAddress);
+      // console.log(this.scpContracct.)
     }
   
     render() {
-      if (this.state.liked) {
-        return 'You liked this.';
+      if (this.state.loading) {
+        return <p>Loading..</p>;
       }
-  
+      
       return (
         <button onClick={() => this.setState({ liked: true })}>
           Like
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config =  {
+    spcContractAddress: '0xadd1D98406082a23615a043207d8e45388d59724'
+};
+
+export default config;
