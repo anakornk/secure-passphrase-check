ea2d26248ebf8a13b89e8bd51bda69bcdafbb72f
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-11 13:17:11 +0800

    fix bugs

--- migrations/2_deploy_spcContract.js
+++ migrations/2_deploy_spcContract.js
@@ -1,8 +1,5 @@
 var SecurePassphraseCheck = artifacts.require("./SecurePassphraseCheck.sol");
-var BasicToken = artifacts.require("./BasicToken.sol");
-var ERC20Prize = artifacts.require("./ERC20Prize.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SecurePassphraseCheck);
-  deployer.deploy(BasicToken);
 };
