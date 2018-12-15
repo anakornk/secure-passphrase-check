ded122f57be9611917eb57bef3d905e5fe32accc
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-16 01:47:56 +0800

    config file must be manually set

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  spcContractAddress: "0xb8aA4Ad9D39763D42c81ed092c4dE99Cb69a442a",
-  basicTokenAddress: "0x492B24f2A9aFB7F8Bb2e4228E64967fF40Dd236b",
-  prizeCreatorAddress: "0x91a9706C80FA6d48186cB51986275161a42F1Dc2"
-};
-
-export default config;
